b3a15a46f0f297f7e543c27b0f6c71127d94a55b
Gourav Shaw <103358572+gourav-shaw@users.noreply.github.com>
2026-07-22 01:06:52 +0530

    LC-single-number [Runtime: N/A; Memory: N/A]

--- array/single-number.cpp
+++ array/single-number.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         int ans=0;
-        int st=0,end=nums.size()-1;
-        if(nums.size()==1)  return nums[nums.size()-1];
-        while(st<=end){
-            ans=nums[st]^nums[end];
-            if(ans>0 && nums[st+1]^nums[end]!=ans){
-                ans=nums[st];
-                break;
-            }
-            else {
-                ans=nums[end];
-                break;
-            }
-            st++;
-            end--;
+        for (int num:nums){
+            ans=ans^num;
         }
         return ans;
     }
